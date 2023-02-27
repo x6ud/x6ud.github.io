52c9a19f9934a631da57d19d7b3496abfe4ca7ff
x6ud <x6ud@foxmail.com>
2023-02-27 15:05:54 +0800

    Added more data

---
+++ human-anatomy-for-artist-search-helper/assets/Editor.ed1898d0.js
@@ -0,0 +1,153 @@
+import{N as ie,g as Z,P as se,a as ae}from"./PhotoDataset.b6619081.js";import{r as A,h as le,a as D,c as g,b as $,N as G,k as ne,l as V,q as F,L as ce,ab as de}from"./feature-buffers.be4220ab.js";import{m as r,e as R,s as w,g as b,i as ue,j as ge,r as O,o as T,D as j,G as y,I as N,f as W,E as H,F as X,J as pe,H as fe,O as he,P as ve,k as Y}from"./vendor.50301a75.js";import{W as U,f as P}from"./Warning.50ec4495.js";import{a as me}from"./index.d4cb29f4.js";var J=A("error",r("svg",{viewBox:"0 0 48 48",version:"1.1",xmlns:"http://www.w3.org/2000/svg"},r("g",{stroke:"none","stroke-width":"1","fill-rule":"evenodd"},r("g",{"fill-rule":"nonzero"},r("path",{d:"M24,4 C35.045695,4 44,12.954305 44,24 C44,35.045695 35.045695,44 24,44 C12.954305,44 4,35.045695 4,24 C4,12.954305 12.954305,4 24,4 Z M17.8838835,16.1161165 L17.7823881,16.0249942 C17.3266086,15.6583353 16.6733914,15.6583353 16.2176119,16.0249942 L16.1161165,16.1161165 L16.0249942,16.2176119 C15.6583353,16.6733914 15.6583353,17.3266086 16.0249942,17.7823881 L16.1161165,17.8838835 L22.233,24 L16.1161165,30.1161165 L16.0249942,30.2176119 C15.6583353,30.6733914 15.6583353,31.3266086 16.0249942,31.7823881 L16.1161165,31.8838835 L16.2176119,31.9750058 C16.6733914,32.3416647 17.3266086,32.3416647 17.7823881,31.9750058 L17.8838835,31.8838835 L24,25.767 L30.1161165,31.8838835 L30.2176119,31.9750058 C30.6733914,32.3416647 31.3266086,32.3416647 31.7823881,31.9750058 L31.8838835,31.8838835 L31.9750058,31.7823881 C32.3416647,31.3266086 32.3416647,30.6733914 31.9750058,30.2176119 L31.8838835,30.1161165 L25.767,24 L31.8838835,17.8838835 L31.9750058,17.7823881 C32.3416647,17.3266086 32.3416647,16.6733914 31.9750058,16.2176119 L31.8838835,16.1161165 L31.7823881,16.0249942 C31.3266086,15.6583353 30.6733914,15.6583353 30.2176119,16.0249942 L30.1161165,16.1161165 L24,22.233 L17.8838835,16.1161165 L17.7823881,16.0249942 L17.8838835,16.1161165 Z"}))))),K=A("info",r("svg",{viewBox:"0 0 28 28",version:"1.1",xmlns:"http://www.w3.org/2000/svg"},r("g",{stroke:"none","stroke-width":"1","fill-rule":"evenodd"},r("g",{"fill-rule":"nonzero"},r("path",{d:"M14,2 C20.6274,2 26,7.37258 26,14 C26,20.6274 20.6274,26 14,26 C7.37258,26 2,20.6274 2,14 C2,7.37258 7.37258,2 14,2 Z M14,11 C13.4477,11 13,11.4477 13,12 L13,12 L13,20 C13,20.5523 13.4477,21 14,21 C14.5523,21 15,20.5523 15,20 L15,20 L15,12 C15,11.4477 14.5523,11 14,11 Z M14,6.75 C13.3096,6.75 12.75,7.30964 12.75,8 C12.75,8.69036 13.3096,9.25 14,9.25 C14.6904,9.25 15.25,8.69036 15.25,8 C15.25,7.30964 14.6904,6.75 14,6.75 Z"}))))),Q=A("success",r("svg",{viewBox:"0 0 48 48",version:"1.1",xmlns:"http://www.w3.org/2000/svg"},r("g",{stroke:"none","stroke-width":"1","fill-rule":"evenodd"},r("g",{"fill-rule":"nonzero"},r("path",{d:"M24,4 C35.045695,4 44,12.954305 44,24 C44,35.045695 35.045695,44 24,44 C12.954305,44 4,35.045695 4,24 C4,12.954305 12.954305,4 24,4 Z M32.6338835,17.6161165 C32.1782718,17.1605048 31.4584514,17.1301307 30.9676119,17.5249942 L30.8661165,17.6161165 L20.75,27.732233 L17.1338835,24.1161165 C16.6457281,23.6279612 15.8542719,23.6279612 15.3661165,24.1161165 C14.9105048,24.5717282 14.8801307,25.2915486 15.2749942,25.7823881 L15.3661165,25.8838835 L19.8661165,30.3838835 C20.3217282,30.8394952 21.0415486,30.8698693 21.5323881,30.4750058 L21.6338835,30.3838835 L32.6338835,19.3838835 C33.1220388,18.8957281 33.1220388,18.1042719 32.6338835,17.6161165 Z"})))));const ye=e=>{const{infoColor:o,successColor:l,warningColor:a,errorColor:i,textColor2:s,progressRailColor:n,fontSize:f,fontWeight:c}=e;return{fontSize:f,fontSizeCircle:"28px",fontWeightCircle:c,railColor:n,railHeight:"8px",iconSizeCircle:"36px",iconSizeLine:"18px",iconColor:o,iconColorInfo:o,iconColorSuccess:l,iconColorWarning:a,iconColorError:i,textColorCircle:s,textColorLineInner:"rgb(255, 255, 255)",textColorLineOuter:s,fillColor:o,fillColorInfo:o,fillColorSuccess:l,fillColorWarning:a,fillColorError:i,lineBgProcessing:"linear-gradient(90deg, rgba(255, 255, 255, .3) 0%, rgba(255, 255, 255, .5) 100%)"}},Ce={name:"Progress",common:le,self:ye};var be=Ce,xe=D([g("progress",{display:"inline-block"},[g("progress-icon",`
+ color: var(--icon-color);
+ transition: color .3s var(--bezier);
+ `),$("line",`
+ width: 100%;
+ display: block;
+ `,[g("progress-content",`
+ display: flex;
+ align-items: center;
+ `,[g("progress-graph",{flex:1})]),g("progress-custom-content",{marginLeft:"14px"}),g("progress-icon",`
+ width: 30px;
+ padding-left: 14px;
+ height: var(--icon-size-line);
+ line-height: var(--icon-size-line);
+ font-size: var(--icon-size-line);
+ `,[$("as-text",`
+ color: var(--text-color-line-outer);
+ text-align: center;
+ width: 40px;
+ font-size: var(--font-size);
+ padding-left: 4px;
+ transition: color .3s var(--bezier);
+ `)])]),$("circle",{width:"120px"},[g("progress-custom-content",`
+ position: absolute;
+ left: 50%;
+ top: 50%;
+ transform: translateX(-50%) translateY(-50%);
+ display: flex;
+ align-items: center;
+ justify-content: center;
+ `),g("progress-text",`
+ position: absolute;
+ left: 50%;
+ top: 50%;
+ transform: translateX(-50%) translateY(-50%);
+ display: flex;
+ align-items: center;
+ color: inherit;
+ font-size: var(--font-size-circle);
+ color: var(--text-color-circle);
+ font-weight: var(--font-weight-circle);
+ transition: color .3s var(--bezier);
+ white-space: nowrap;
+ `),g("progress-icon",`
+ position: absolute;
+ left: 50%;
+ top: 50%;
+ transform: translateX(-50%) translateY(-50%);
+ display: flex;
+ align-items: center;
+ color: var(--icon-color);
+ font-size: var(--icon-size-circle);
+ `)]),$("multiple-circle",{width:"200px",color:"inherit"},[g("progress-text",`
+ font-weight: var(--font-weight-circle);
+ color: var(--text-color-circle);
+ position: absolute;
+ left: 50%;
+ top: 50%;
+ transform: translateX(-50%) translateY(-50%);
+ display: flex;
+ align-items: center;
+ justify-content: center;
+ transition: color .3s var(--bezier);
+ `)]),g("progress-content",{position:"relative"}),g("progress-graph",{position:"relative"},[g("progress-graph-circle",[D("svg",{verticalAlign:"bottom"}),g("progress-graph-circle-fill",`
+ stroke: var(--fill-color);
+ transition:
+ opacity .3s var(--bezier),
+ stroke .3s var(--bezier),
+ stroke-dasharray .3s var(--bezier);
+ `,[$("empty",{opacity:0})]),g("progress-graph-circle-rail",`
+ transition: stroke .3s var(--bezier);
+ overflow: hidden;
+ stroke: var(--rail-color);
+ `)]),g("progress-graph-line",[$("indicator-inside",[g("progress-graph-line-rail",{height:"16px",borderRadius:"10px"},[g("progress-graph-line-fill",{height:"inherit",borderRadius:"10px"}),g("progress-graph-line-indicator",`
+ background: #0000;
+ white-space: nowrap;
+ text-align: right;
+ margin-left: 14px;
+ margin-right: 14px;
+ height: inherit;
+ line-height: 16px;
+ font-size: 12px;
+ color: var(--text-color-line-inner);
+ transition: color .3s var(--bezier);
+ `)])]),$("indicator-inside-label",`
+ height: 16px;
+ display: flex;
+ align-items: center;
+ `,[g("progress-graph-line-rail",{flex:1,transition:"background-color .3s var(--bezier)"}),g("progress-graph-line-indicator",`
+ background: var(--fill-color);
+ font-size: 12px;
+ transform: translateZ(0);
+ display: flex;
+ vertical-align: middle;
+ height: 16px;
+ line-height: 16px;
+ padding: 0 10px;
+ border-radius: 10px;
+ position: absolute;
+ white-space: nowrap;
+ color: var(--text-color-line-inner);
+ transition:
+ right .2s var(--bezier),
+ color .3s var(--bezier),
+ background-color .3s var(--bezier);
+ `)]),g("progress-graph-line-rail",`
+ position: relative;
+ overflow: hidden;
+ height: var(--rail-height);
+ border-radius: 5px;
+ background-color: var(--rail-color);
+ transition: background-color .3s var(--bezier);
+ `,[g("progress-graph-line-fill",`
+ background: var(--fill-color);
+ position: relative;
+ border-radius: 5px;
+ height: inherit;
+ width: 100%;
+ max-width: 0%;
+ transition:
+ background-color .3s var(--bezier),
+ max-width .2s var(--bezier);
+ `,[$("processing",[D("&::after",`
+ content: "";
+ background-image: var(--line-bg-processing);
+ animation: progress-processing-animation 2s var(--bezier) infinite;
+ `)])])])])])]),D("@keyframes progress-processing-animation",`
+ 0% {
+ position: absolute;
+ left: 0;
+ top: 0;
+ bottom: 0;
+ right: 100%;
+ opacity: 1;
+ }
+ 66% {
+ position: absolute;
+ left: 0;
+ top: 0;
+ bottom: 0;
+ right: 0;
+ opacity: 0;
+ }
+ 100% {
+ position: absolute;
+ left: 0;
+ top: 0;
+ bottom: 0;
+ right: 0;
+ opacity: 0;
+ }
+ `)]);const we={success:r(Q,null),error:r(J,null),warning:r(U,null),info:r(K,null)};var ke=R({name:"ProgressLine",props:{clsPrefix:{type:String,required:!0},percentage:{type:Number,default:0},railColor:String,railStyle:[String,Object],fillColor:String,status:{type:String,required:!0},indicatorPlacement:{type:String,required:!0},indicatorTextColor:String,unit:{type:String,default:"%"},processing:{type:Boolean,required:!0},showIndicator:{type:Boolean,required:!0},height:[String,Number],railBorderRadius:[String,Number],fillBorderRadius:[String,Number]},setup(e,{slots:o}){const l=w(()=>P(e.height)),a=w(()=>e.railBorderRadius!==void 0?P(e.railBorderRadius):e.height!==void 0?P(e.height,{c:.5}):""),i=w(()=>e.fillBorderRadius!==void 0?P(e.fillBorderRadius):e.railBorderRadius!==void 0?P(e.railBorderRadius):e.height!==void 0?P(e.height,{c:.5}):"");return()=>{const{indicatorPlacement:s,railColor:n,railStyle:f,percentage:c,unit:t,indicatorTextColor:p,status:v,showIndicator:d,fillColor:u,processing:S,clsPrefix:h}=e;return r("div",{class:`${h}-progress-content`,role:"none"},r("div",{class:`${h}-progress-graph`,"aria-hidden":!0},r("div",{class:[`${h}-progress-graph-line`,{[`${h}-progress-graph-line--indicator-${s}`]:!0}]},r("div",{class:`${h}-progress-graph-line-rail`,style:[{backgroundColor:n,height:l.value,borderRadius:a.value},f]},r("div",{class:[`${h}-progress-graph-line-fill`,S&&`${h}-progress-graph-line-fill--processing`],style:{maxWidth:`${e.percentage}%`,backgroundColor:u,height:l.value,borderRadius:i.value}},s==="inside"?r("div",{class:`${h}-progress-graph-line-indicator`},`${c}${t}`):null)))),d&&s==="outside"?r("div",null,o.default?r("div",{class:`${h}-progress-custom-content`,style:{color:p},role:"none"},o.default()):v==="default"?r("div",{role:"none",class:`${h}-progress-icon ${h}-progress-icon--as-text`,style:{color:p}},c,t):r("div",{class:`${h}-progress-icon`,"aria-hidden":!0},r(G,{clsPrefix:h},{default:()=>we[v]}))):null)}}});const $e={success:r(Q,null),error:r(J,null),warning:r(U,null),info:r(K,null)};var Se=R({name:"ProgressCircle",props:{clsPrefix:{type:String,required:!0},status:{type:String,required:!0},strokeWidth:{type:Number,required:!0},fillColor:String,railColor:String,railStyle:[String,Object],percentage:{type:Number,default:0},showIndicator:{type:Boolean,reqiuired:!0},indicatorTextColor:String,unit:String,viewBoxWidth:{type:Number,required:!0}},setup(e,{slots:o}){const l=w(()=>`${Math.PI*e.percentage}, ${e.viewBoxWidth*8}`);return()=>{const{fillColor:a,railColor:i,railStyle:s,strokeWidth:n,status:f,percentage:c,showIndicator:t,indicatorTextColor:p,unit:v,clsPrefix:d}=e;return r("div",{class:`${d}-progress-content`,role:"none"},r("div",{class:`${d}-progress-graph`,"aria-hidden":!0},r("div",{class:`${d}-progress-graph-circle`},r("svg",{viewBox:"0 0 110 110"},r("g",null,r("path",{class:`${d}-progress-graph-circle-rail`,d:"m 55 5 a 50 50 0 1 1 0 100 a 50 50 0 1 1 0 -100","stroke-width":n*1.1,"stroke-linecap":"round",fill:"none",style:[{strokeDashoffset:0,stroke:i},s]})),r("g",null,r("path",{class:[`${d}-progress-graph-circle-fill`,c===0&&`${d}-progress-graph-circle-fill--empty`],d:"m 55 5 a 50 50 0 1 1 0 100 a 50 50 0 1 1 0 -100","stroke-width":n*1.1,"stroke-linecap":"round",fill:"none",style:{strokeDasharray:l.value,strokeDashoffset:0,stroke:a}}))))),t?r("div",null,o.default?r("div",{class:`${d}-progress-custom-content`,role:"none"},o.default()):f!=="default"?r("div",{class:`${d}-progress-icon`,"aria-hidden":!0},r(G,{clsPrefix:d},{default:()=>$e[f]})):r("div",{class:`${d}-progress-text`,style:{color:p},role:"none"},r("span",{class:`${d}-progress-text__percentage`},c),r("span",{class:`${d}-progress-text__unit`},v))):null)}}});function ee(e,o,l=100){return`m ${l/2} ${l/2-e} a ${e} ${e} 0 1 1 0 ${2*e} a ${e} ${e} 0 1 1 0 -${2*e}`}var Le=R({name:"ProgressMultipleCircle",props:{clsPrefix:{type:String,required:!0},viewBoxWidth:{type:Number,required:!0},percentage:{type:Array,default:[0]},strokeWidth:{type:Number,required:!0},circleGap:{type:Number,required:!0},showIndicator:{type:Boolean,required:!0},fillColor:{type:Array,default:()=>[]},railColor:{type:Array,default:()=>[]},railStyle:{type:Array,default:()=>[]}},setup(e,{slots:o}){const l=w(()=>e.percentage.map((i,s)=>`${Math.PI*i/100*(e.viewBoxWidth/2-e.strokeWidth/2*(1+2*s)-e.circleGap*s)*2}, ${e.viewBoxWidth*8}`));return()=>{const{viewBoxWidth:a,strokeWidth:i,circleGap:s,showIndicator:n,fillColor:f,railColor:c,railStyle:t,percentage:p,clsPrefix:v}=e;return r("div",{class:`${v}-progress-content`,role:"none"},r("div",{class:`${v}-progress-graph`,"aria-hidden":!0},r("div",{class:`${v}-progress-graph-circle`},r("svg",{viewBox:`0 0 ${a} ${a}`},p.map((d,u)=>r("g",{key:u},r("path",{class:`${v}-progress-graph-circle-rail`,d:ee(a/2-i/2*(1+2*u)-s*u,i,a),"stroke-width":i,"stroke-linecap":"round",fill:"none",style:[{strokeDashoffset:0,stroke:c[u]},t[u]]}),r("path",{class:[`${v}-progress-graph-circle-fill`,d===0&&`${v}-progress-graph-circle-fill--empty`],d:ee(a/2-i/2*(1+2*u)-s*u,i,a),"stroke-width":i,"stroke-linecap":"round",fill:"none",style:{strokeDasharray:l.value[u],strokeDashoffset:0,stroke:f[u]}})))))),n&&o.default?r("div",null,r("div",{class:`${v}-progress-text`},o.default())):null)}}});const Pe=Object.assign(Object.assign({},V.props),{processing:Boolean,type:{type:String,default:"line"},status:{type:String,default:"default"},railColor:[String,Array],railStyle:[String,Array],color:[String,Array],viewBoxWidth:{type:Number,default:100},strokeWidth:{type:Number,default:7},percentage:[Number,Array],unit:{type:String,default:"%"},showIndicator:{type:Boolean,default:!0},indicatorPosition:{type:String,default:"outside"},indicatorPlacement:{type:String,default:"outside"},indicatorTextColor:String,circleGap:{type:Number,default:1},height:Number,borderRadius:[String,Number],fillBorderRadius:[String,Number]});var ze=R({name:"Progress",props:Pe,setup(e){const o=w(()=>e.indicatorPlacement||e.indicatorPosition),{mergedClsPrefixRef:l}=ne(e),a=V("Progress","Progress",xe,be,e,l);return{mergedClsPrefix:l,mergedIndicatorPlacement:o,cssVars:w(()=>{const{status:i}=e,{common:{cubicBezierEaseInOut:s},self:{fontSize:n,fontSizeCircle:f,railColor:c,railHeight:t,iconSizeCircle:p,iconSizeLine:v,textColorCircle:d,textColorLineInner:u,textColorLineOuter:S,lineBgProcessing:h,fontWeightCircle:k,[F("iconColor",i)]:z,[F("fillColor",i)]:C}}=a.value;return{"--bezier":s,"--fill-color":C,"--font-size":n,"--font-size-circle":f,"--font-weight-circle":k,"--icon-color":z,"--icon-size-circle":p,"--icon-size-line":v,"--line-bg-processing":h,"--rail-color":c,"--rail-height":t,"--text-color-circle":d,"--text-color-line-inner":u,"--text-color-line-outer":S}})}},render(){const{type:e,cssVars:o,indicatorTextColor:l,showIndicator:a,status:i,railColor:s,railStyle:n,color:f,percentage:c,viewBoxWidth:t,strokeWidth:p,mergedIndicatorPlacement:v,unit:d,borderRadius:u,fillBorderRadius:S,height:h,processing:k,circleGap:z,mergedClsPrefix:C,$slots:B}=this;return r("div",{class:[`${C}-progress`,`${C}-progress--${e}`,`${C}-progress--${i}`],style:o,"aria-valuemax":100,"aria-valuemin":0,"aria-valuenow":c,role:e==="circle"||e==="line"?"progressbar":"none"},e==="circle"?r(Se,{clsPrefix:C,status:i,showIndicator:a,indicatorTextColor:l,railColor:s,fillColor:f,railStyle:n,percentage:c,viewBoxWidth:t,strokeWidth:p,unit:d},B):e==="line"?r(ke,{clsPrefix:C,status:i,showIndicator:a,indicatorTextColor:l,railColor:s,fillColor:f,railStyle:n,percentage:c,processing:k,indicatorPlacement:v,unit:d,fillBorderRadius:S,railBorderRadius:u,height:h},B):e==="multiple-circle"?r(Le,{clsPrefix:C,strokeWidth:p,railColor:s,fillColor:f,railStyle:n,viewBoxWidth:t,percentage:c,showIndicator:a,circleGap:z},B):null)}}),Be=R({components:{NButton:ce,NInput:ie,NSpin:de,NProgress:ze},setup(){const e=new se,o=b(!1),l=b(0),a=b(0),i=b("1"),s=b("1");let n=60;const f=b(!1),c=b(!1),t=b(0),p=b(0),v=w(function(){return Math.round(p.value/t.value*100)}),d=w(function(){return`${p.value} / ${t.value}`}),u=b(0),S=w(function(){function x(L){return L<10?"0"+L:L+""}let m=Math.round(u.value);const M=m%60;m=Math.floor(m/60);const _=m%60;return m=Math.floor(m/60),`${x(m)}:${x(_)}:${x(M)}`}),h=b([]);ue(async function(){try{o.value=!0,await e.load(),await e.loadAllChunks(),l.value=e.size;const x=await Z(1);a.value=x.numOfPages,s.value=x.numOfPages+"",n=x.photos.length||n}finally{o.value=!1}});let k=0;function z(){k||(k=setInterval(function(){u.value=Math.max(0,u.value-1)},1e3))}function C(){k&&(clearInterval(k),k=0)}ge(function(){C()});async function B(){var x;try{f.value=!0,t.value=0,p.value=0,u.value=0,c.value=!1;const m=Number(i.value),M=Number(s.value);t.value=n*(M-m+1),u.value=15*t.value,z();let _=15e3;for(let I=M;I>=m&&!c.value;--I){const L=await Z(I);L.photos.length<n&&(t.value-=n-L.photos.length);for(let E of L.photos){if(!await e.has(E.id)){console.log(`Processing #${E.id} in page ${I}`);const te=Date.now(),q=new ae;if(await q.loadByWorker(E.id),h.value.push(q),(x=q.normalized)==null?void 0:x.length){await e.add(q),await e.writeToFile(),l.value+=1;let oe=Date.now()-te;p.value>1&&(_=(_*p.value+oe)/(p.value+1))}}if(p.value+=1,c.value||t.value===p.value)break;u.value=Math.round(_/1e3*(t.value-p.value))}}}catch(m){console.error(m),alert("An error occurred: "+m)}C(),f.value=!1}return{dataset:e,datasetLoading:o,datasetLength:l,totalPages:a,pageStart:i,pageEnd:s,processing:f,stop:c,total:t,progress:p,percent:v,progressText:d,remainingSecs:u,remaining:S,detectResult:h,process:B}}});const re=e=>(he("data-v-1f8922ca"),e=e(),ve(),e),_e={class:"container rows"},Ie={class:"rows fill"},Re={class:"cols",style:{"font-size":"12px","align-items":"center"}},Ne=re(()=>y("div",{class:"fill"},null,-1)),We=re(()=>y("span",null,"~",-1)),Me=Y(" Crawl "),qe={class:"scroll-list fill"},De=["data-id"],Te=["src"],je={key:0,class:"progress"},Ee={class:"cols",style:{"margin-bottom":"8px","align-items":"center"}},Ae={style:{"margin-right":"4px"}},Oe={style:{"font-size":"12px"}};function Ze(e,o,l,a,i,s){const n=O("n-input"),f=O("n-button"),c=O("n-progress");return T(),j(X,null,[y("div",_e,[y("div",Ie,[y("div",Re,[y("span",null,"Num of Records: "+N(e.datasetLength),1),Ne,y("span",null,"Total "+N(e.totalPages)+" / Page\xA0",1),W(n,{style:{width:"60px"},size:"tiny",placeholder:"Page Start",value:e.pageStart,"onUpdate:value":o[0]||(o[0]=t=>e.pageStart=t)},null,8,["value"]),We,W(n,{style:{width:"60px"},size:"tiny",placeholder:"Page End",value:e.pageEnd,"onUpdate:value":o[1]||(o[1]=t=>e.pageEnd=t)},null,8,["value"]),W(f,{type:"primary",size:"tiny",onClick:e.process},{default:H(()=>[Me]),_:1},8,["onClick"])]),y("div",qe,[(T(!0),j(X,null,pe(e.detectResult,t=>(T(),j("div",{class:"item photo","data-id":t.id},[y("img",{src:t.landmarksImage||t.url,alt:"",referrerpolicy:"no-referrer"},null,8,Te)],8,De))),256))])])]),e.processing?(T(),j("div",je,[y("div",Ee,[W(c,{class:"fill",type:"line",percentage:e.percent,"indicator-placement":"inside",processing:""},null,8,["percentage"]),y("div",Ae,N(e.progressText),1),W(f,{onClick:o[2]||(o[2]=t=>e.stop=!0),disabled:e.stop,size:"tiny"},{default:H(()=>[Y(N(e.stop?"Stopping":"Stop"),1)]),_:1},8,["disabled"])]),y("div",Oe," Remaining: "+N(e.remaining),1)])):fe("",!0)],64)}var Ye=me(Be,[["render",Ze],["__scopeId","data-v-1f8922ca"]]);export{Ye as default};
+//# sourceMappingURL=Editor.ed1898d0.js.map
